6bf040249fad1406500a098b98f0822e6e51a4b4
Vladislav Griaztsov <vladislavgriaztsov@MacBook-Pro-Vladislav.local>
2024-04-12 12:41:21 +0300

    first func

---
+++ sudoku.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+void input(int *matrix) {
+    int diff;
+    scanf("%d", &diff);
+    switch (diff){
+        case 1;
+    }
+}
+int main(void) {
+    int *matrix[9][9];
+
+    return 0;
+}
